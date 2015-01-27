1dd85d5a2afcda07370da36e06f630e60c8e849a
Ryan budny <rb1econ@gmail.com>
2015-01-27 11:06:38 -0700

    clock ticks

--- main.js
+++ main.js
@@ -16,19 +16,35 @@ $(document).on('ready', function() {
   
 
   var clockAmPm = $('<div class="clock-ampm"></div>')
-  		
-
+  
   var clockText = $('<div class="clock-text"></div>')
-  		.text('05:00')
+  var time = 0
+  var countTime = 0		
+  setInterval(function(){
+                      time = countTime++;
+                      // console.log(time);
+                      $('.clock-text').text(time);
+                      // return time;
+                    }, 1000);
+  
+
+// moment js to format time ;;;; use the date property
+console.log('Time: ', time);
+  
+  // var clockText = $('<div class="clock-text"></div>')
+                    
+
+  
+  		
   		
 
-  var bottomAM = $('<div class="bottom-am"><p class="right">AM</p></div>')
-  		.text('53 60 70 90 110 140 170')
+  var bottomAM = $('<div class="bottom-am"><span class="right-label">AM</span>53 60 70 90 110 140 170</div>')
+  		// .text('53 60 70 90 110 140 170')
   		
   		
 
-  var bottomFM = $('<div class="bottom-fm"></div>')
-  		.text('FM 88 92 96 102 106 108')
+  var bottomFM = $('<div class="bottom-fm"><span class="right-label">FM</span>88 92 96 102 106 108</div>')
+  		// .text('FM 88 92 96 102 106 108')
   		
 
   clockScreen.append(bottomFM);
@@ -41,7 +57,7 @@ $(document).on('ready', function() {
   
   $('body').append(outerShell);
 
-
+  $()
 
 
 
